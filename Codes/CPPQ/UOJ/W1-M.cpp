e88cc2780bb6521b8ceaf3d48320fc53c6649482
Loid <2232068231@qq.com>
2022-10-08 15:44:55 +0800

    UOJ W-1 Update

--- Codes/CPPQ/UOJ/W1-M.cpp
+++ Codes/CPPQ/UOJ/W1-M.cpp
@@ -4,9 +4,13 @@ using namespace std;
 
 int main()
 {
-    int h1, m1, h2, m2, diff;
+    int h1, m1, h2, m2, t1, t2, diff;
     cin >> h1 >> m1 >> h2 >> m2;
-    diff = h2 * 3600 + m2 - h1 * 3600 - m2;
+    t1 = h1 * 60 + m1;
+    t2 = h2 * 60 + m2;
+    if (t2 <= t1) //注意带等号，走完一整天（24h）的情况。
+        t2 += 1440;
+    diff = t2 - t1;
     cout << diff << endl;
     return 0;
 }
