8a5746b26a1d8b810fa0578edc9423920005517f
Lozumi Everkine <fantasynico1026@gmail.com>
2022-11-06 00:53:19 +0800

    UOJ W4 Update

--- Codes/CPPQ/UOJ/W4/4B-3.cpp
+++ Codes/CPPQ/UOJ/W4/4B-3.cpp
@@ -1,43 +1,55 @@
-// UOJ W4-B-2
+// UOJ W4-B-3
 //简化为字符数量统计
 #include <iostream>
-#include <cstring>
-#include <ctime>
+#include <string>
+//#include <ctime>
 
 using namespace std;
-void maopao(int a[], int n);
+void quick_sort(int a[], int l, int r);
 
 int main(void)
 {
-    int t1 = clock();
-    char str[100];   //字符串个数不可超过50个字符
-    int a[28] = {0}; //存放每个字母出现的次数，下标从0到25分别与字母a至z对应
+    // int t1 = clock();
 
-    cin >> str;            //输入字符串
-    int len = strlen(str); //用整型变量len存放输入的字符串的长度，若想使用此句，需包含头文件<cstring>
+    string str;
+    getline(cin, str);
+    // transform(str.begin(), str.end(), str.begin(), ::tolower); //将所有字母转为小写
 
-    //修改每个字母对应的出现次数
-    for (int i = 0; i < len; i++)
-        a[str[i] - 'a']++; //例如，出现字母c，c-'a'为2，则a[2]增长1
+    int cnt[26] = {};
+    for (int i = 0; i < str.size(); i++)
+    {
+        cnt[(int)str[i] - 'a']++;
+    }
+    quick_sort(cnt, 0, 25);
+    cout << cnt[25] << endl;
 
-    maopao(a, 28);
-    cout << a[0] << endl;
-
-    int t2 = clock();
-    cout << t2 - t1 << endl;
+    // int t2 = clock();
+    // cout << t2 - t1 << endl;
     return 0;
 }
 
-void maopao(int a[], int n)
+void quick_sort(int a[], int l, int r)
 {
-    for (int i = 0; i < n - 1; i++)
+    if (l < r)
     {
-        for (int j = 0; j < n - i - 1; j++)
+        int i, j, x;
+
+        i = l;
+        j = r;
+        x = a[i];
+        while (i < j)
         {
-            if (a[j] > a[j + 1])
-            {
-                swap(a[j], a[j + 1]);
-            }
+            while (i < j && a[j] > x)
+                j--; // 从右向左找第一个小于x的数
+            if (i < j)
+                a[i++] = a[j];
+            while (i < j && a[i] < x)
+                i++; // 从左向右找第一个大于x的数
+            if (i < j)
+                a[j--] = a[i];
         }
+        a[i] = x;
+        quick_sort(a, l, i - 1); /* 递归调用 */
+        quick_sort(a, i + 1, r); /* 递归调用 */
     }
 }
