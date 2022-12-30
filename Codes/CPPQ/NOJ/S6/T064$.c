8912af30cd929f3d2fc146e0540c026ef7069519
Lozumi Everkine <fantasynico1026@gmail.com>
2022-12-30 22:37:56 +0800

    NOJ S6 Finished

---
+++ Codes/CPPQ/NOJ/S6/T064$.c
@@ -0,0 +1,14 @@
+// NOJ T064 合并字符串
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char a[100], b[100];
+    gets(a);
+    gets(b);
+    strncat(a, b, 100);
+    puts(a);
+    return 0;
+}
---
+++ Codes/CPPQ/NOJ/S6/T067$.cpp
@@ -0,0 +1,63 @@
+// NOJ T067 Music
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    char S[10];
+    gets(S);
+    if (S[0] == 'A' && S[1] == '#')
+    {
+        S[0] = 'B', S[1] = 'b';
+        cout << S << endl;
+    }
+    else if (S[0] == 'C' && S[1] == '#')
+    {
+        S[0] = 'D', S[1] = 'b';
+        cout << S << endl;
+    }
+    else if (S[0] == 'D' && S[1] == '#')
+    {
+        S[0] = 'E', S[1] = 'b';
+        cout << S << endl;
+    }
+    else if (S[0] == 'F' && S[1] == '#')
+    {
+        S[0] = 'G', S[1] = 'b';
+        cout << S << endl;
+    }
+    else if (S[0] == 'G' && S[1] == '#')
+    {
+        S[0] = 'A', S[1] = 'b';
+        cout << S << endl;
+    }
+    else if (S[0] == 'B' && S[1] == 'b')
+    {
+        S[0] = 'A', S[1] = '#';
+        cout << S << endl;
+    }
+    else if (S[0] == 'D' && S[1] == 'b')
+    {
+        S[0] = 'C', S[1] = '#';
+        cout << S << endl;
+    }
+    else if (S[0] == 'E' && S[1] == 'b')
+    {
+        S[0] = 'D', S[1] = '#';
+        cout << S << endl;
+    }
+    else if (S[0] == 'G' && S[1] == 'b')
+    {
+        S[0] = 'F', S[1] = '#';
+        cout << S << endl;
+    }
+    else if (S[0] == 'A' && S[1] == 'b')
+    {
+        S[0] = 'G', S[1] = '#';
+        cout << S << endl;
+    }
+    else
+        cout << "unique" << endl;
+    return 0;
+}
